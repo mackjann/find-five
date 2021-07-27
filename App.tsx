e35123a8c8dae1f839f86b68eb8d34cd717d0eef
Jack Mann <mackjann>
2021-07-27 12:40:21 +0100

    got navigation working with typescript, still some bugs to fix but not affecting performance

--- App.tsx
+++ App.tsx
@@ -1,24 +1,20 @@
-import { StatusBar } from "expo-status-bar";
-import styles from "./styles.js";
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
 import "react-native-gesture-handler";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-// import Home from "./screens/Home";
+import Home from "./screens/Home";
+import JaneProfile from "./screens/JaneProfile";
 
 const Stack = createStackNavigator();
 
-export default function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        {/* <Stack.Screen
-          name="Home"
-          component={Home}
-          options={{ title: "Welcome" }}
-        /> */}
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+export default function App(): any {
+	return (
+		<NavigationContainer>
+			<Stack.Navigator>
+				<Stack.Screen name="Home" component={Home} />
+				<Stack.Screen name="Profile" component={JaneProfile} />
+			</Stack.Navigator>
+		</NavigationContainer>
+	);
 }
--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,42 +1,53 @@
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import React from "react";
 import "react-native-gesture-handler";
-import { Text } from "react-native";
+import {
+	Text,
+	SafeAreaView,
+	ScrollView,
+	Button,
+	StatusBar,
+} from "react-native";
 import styles from "../styles.js";
 import firebase from "../config.js";
 
 const ref = firebase.firestore().collection("users");
+const getEmail = () =>
+	ref.onSnapshot(({ docs }) => {
+		docs.forEach((doc) => {
+			console.log(doc.data());
+		});
+	});
 
-// const Home = ({ navigation }) => {
-//   const getEmail = () =>
-//     ref.onSnapshot(({ docs }); => {
-//       docs.forEach((doc) => {
-//         console.log(doc.data());
-//         console.log("1");
-//       });
-//     });
+const Home = ({ navigation }: any): JSX.Element => {
+	return (
+		<SafeAreaView style={styles.container}>
+			<ScrollView showsVerticalScrollIndicator={false}>
+				<Text
+					style={{
+						textAlign: "center",
+						fontWeight: "bold",
+						fontSize: 18,
+						marginTop: 30,
+						marginBottom: 30,
+						width: 200,
+					}}
+				>
+					CYBER-DRIP
+				</Text>
 
-//   return (
-//     <SafeAreaView style={styles.container}>
-//       <ScrollView
-//         style={styles.scrollView}
-//         showsVerticalScrollIndicator={false}
-//       >
-//         <Text
-//           style={{
-//             textAlign: "center",
-//             fontWeight: "bold",
-//             fontSize: 18,
-//             marginTop: 30,
-//             marginBottom: 30,
-//             width: 200,
-//           }}
-//         >
-//           CYBER-DRIP
-//         </Text>
+				<Button
+					title="Go to Jane's profile"
+					onPress={() => navigation.navigate("Profile", { name: "Jane" })}
+				/>
 
-//         <StatusBar style="auto" />
-//       </ScrollView>
-//     </SafeAreaView>
-//   );
-// };
+				<Button title="GetEmail" onPress={getEmail} />
 
-// export default Home;
+				<StatusBar />
+			</ScrollView>
+		</SafeAreaView>
+	);
+};
+
+export default Home;
