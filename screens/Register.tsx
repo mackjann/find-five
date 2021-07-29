66543cb867c43b19e96e515f0710b797cf078953
Jack Mann <mackjann>
2021-07-29 16:33:42 +0100

    creating a user document now links it to user auth by uid, can use auth uid to access user data

--- screens/Register.tsx
+++ screens/Register.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import "react-native-gesture-handler";
