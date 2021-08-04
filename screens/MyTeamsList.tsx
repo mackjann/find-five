37f858318a51fd9f1d2559136b55eec7a5d71abe
maria-walker <mariawalker5@ymail.com>
2021-08-04 12:19:16 +0100

    MyTeams and MyProfile - cards for team info

--- screens/MyTeamsList.tsx
+++ screens/MyTeamsList.tsx
@@ -15,186 +15,221 @@ import {
 } from "react-native";
 import styles from "../styles.js";
 import { useState, useEffect } from "react";
+import { getUsersTeams } from "../utils.js";
+import firebase from "../config";
+import "firebase/auth";
 
 LogBox.ignoreLogs(["Setting a timer for a long period"]);
 LogBox.ignoreLogs(["VirtualizedLists should never be nested"]);
 LogBox.ignoreLogs(["VirtualizedList: missing keys for items"]);
+LogBox.ignoreLogs(["source.uri should not be an empty string"]);
 
-const MyTeams = ({ navigation, route }: any): JSX.Element => {
-	// const { teamName } = route.params;
-	// const { teams } = route.params;
+const MyTeams = ({ navigation }: any): JSX.Element => {
+	interface Teams {
+		[index: number]: {
+			id: string;
+			pic: string;
+			teamName: string;
+			location: string;
+		};
+	}
+	const userID: null | string = firebase.auth().currentUser.uid;
+	const [teams, setTeams] = React.useState([
+		{ id: "", pic: "", teamName: "", location: "" },
+	]);
 
-	// const [team, setTeam] = useState({});
+	useEffect(() => {
+		getUsersTeams(userID).then((res) => {
+			setTeams(res);
+		});
+	}, []);
 
-	// useEffect(
-	// 	() =>
-	// 		teams.forEach((footieTeam) => {
-	// 			if (footieTeam.teamName === teamName) {
-	// 				setTeam(footieTeam);
-	// 				console.log(footieTeam);
-	// 			}
-	// 		}),
-
-	// 	[]
-	// );
-
-	const team = {
-		admin: "8S5DTo8rbrb2aog1kLG9",
-		availability: {
-			fridayAM: true,
-			fridayPM: true,
-			mondayAM: true,
-			mondayPM: true,
-			saturdayAM: true,
-			saturdayPM: true,
-			sundayAM: true,
-			sundayPM: true,
-			thursdayAM: true,
-			thursdayPM: true,
-			tuesdayAM: true,
-			tuesdayPM: true,
-			wednesdayAM: true,
-			wednesdayPM: true,
-		},
-		bio: "we da best!",
-		lat: 53.42521,
-		lng: -2.23027,
-		lookingFor: {
-			DEF: false,
-			FWD: true,
-			GK: true,
-			MID: false,
-		},
-		teamName: "cyber drip",
-		teamPic: true,
-		venue: "Manchester",
-		venueLocation: "M20",
-	};
+	console.log(teams, "<---- teams");
 
 	return (
 		<SafeAreaView style={styles.container}>
 			<ScrollView showsVerticalScrollIndicator={false}>
-				<Text style={styles.button_text}>⚽My Teams⚽</Text>
-
 				<View
 					style={{
+						flex: 1,
 						flexDirection: "row",
-						justifyContent: "space-between",
-						alignItems: "flex-start",
-						width: 300,
-						backgroundColor: "rgba(250,250,250, 0.5)",
-						borderRadius: 20,
-						alignSelf: "center",
-						padding: 10,
-						margin: 10,
-
-						// height: Dimensions.get("window").height * 0.15,
+						justifyContent: "center",
+						marginBottom: 10,
+						width: 280,
+						height: 55,
 					}}
 				>
 					<Image
 						style={{
-							marginBottom: 0,
-							alignSelf: "center",
+							width: 40,
+							top: -12,
 						}}
-						resizeMode={"cover"}
-						source={{
-							width: 45,
-							height: 45,
-							uri: "https://logos-world.net/wp-content/uploads/2020/06/England-logo.png",
-						}}
+						resizeMode={"contain"}
+						source={require("../images/find5-icon-no-bg.png")}
 					/>
-					<Text style={{ margin: 5 }}>
-						<Text
-							style={{ fontWeight: "bold", textTransform: "capitalize" }}
-						>{`${team.teamName}`}</Text>
-					</Text>
-					<Text style={{ margin: 5 }}>
-						<Text style={{ fontWeight: "bold" }}>{"Location\n"}</Text>
-						{`${team.venueLocation}`}
-					</Text>
-					<View>
-						<TouchableOpacity
-							style={[
-								styles.small_button,
-								{
-									borderWidth: 2,
-									height: 30,
-									borderRadius: 12,
-									width: 90,
-								},
-							]}
-							onPress={() =>
-								navigation.navigate("ExternalTeam", {
-									teamName: team.teamName,
-									teams: teams,
-								})
-							}
-						>
-							<Text
-								style={[
-									styles.button_text,
-									{ alignSelf: "center", fontSize: 14 },
-								]}
-							>
-								Team Page
-							</Text>
-						</TouchableOpacity>
-
-						<TouchableOpacity
-							style={[
-								styles.small_button,
-								{
-									borderWidth: 2,
-									height: 30,
-									borderRadius: 12,
-									width: 90,
-								},
-							]}
-						>
-							<Text
-								style={[
-									styles.button_text,
-									{ alignSelf: "center", fontSize: 14 },
-								]}
-							>
-								Leave team
-							</Text>
-						</TouchableOpacity>
-					</View>
+					<Text style={styles.button_text}>{` My Teams`}</Text>
 				</View>
 
-				<View
+				{teams.map((team) => {
+					return (
+						<View key={team.id}>
+							<View
+								style={{
+									flexDirection: "row",
+									justifyContent: "space-between",
+									alignItems: "flex-start",
+									width: 320,
+									height: 65,
+									backgroundColor: "rgba(250,250,250, 0.7)",
+									borderRadius: 2,
+									alignSelf: "center",
+									padding: 10,
+									margin: 10,
+								}}
+							>
+								<View
+									style={{
+										marginRight: 5,
+									}}
+								>
+									<Image
+										style={{
+											alignSelf: "center",
+											marginBottom: 6,
+										}}
+										resizeMode={"cover"}
+										source={{
+											width: 50,
+											height: 50,
+											uri: team.pic,
+										}}
+									/>
+								</View>
+								<View
+									style={{
+										justifyContent: "flex-start",
+										alignItems: "flex-start",
+										flexDirection: "column",
+									}}
+								>
+									<View
+										style={{
+											flexDirection: "column",
+											alignItems: "flex-start",
+											paddingBottom: 2,
+											margin: 1,
+											borderBottomWidth: 1,
+											borderBottomColor: "grey",
+											width: 100,
+										}}
+									>
+										<Text
+											style={{
+												fontWeight: "bold",
+												fontSize: 16,
+												textTransform: "capitalize",
+											}}
+										>{`${team.teamName}`}</Text>
+									</View>
+									<Text style={{ margin: 1 }}>
+										<Text style={{ fontWeight: "bold" }}>{"Location:"}</Text>
+										{` ${team.location}`}
+									</Text>
+								</View>
+								<View
+									style={{
+										width: 20,
+									}}
+								></View>
+
+								<View
+									style={{ flexDirection: "row", alignItems: "flex-start" }}
+								>
+									<TouchableOpacity
+										style={[
+											styles.small_button,
+											{
+												borderWidth: 0.5,
+												height: 46,
+												borderRadius: 5,
+												width: 55,
+												margin: 3,
+												marginTop: 0,
+											},
+										]}
+										onPress={() =>
+											navigation.navigate("ExternalTeam", {
+												teamName: team.teamName,
+												teams: teams,
+											})
+										}
+									>
+										<Text
+											style={[
+												styles.button_text,
+												{ alignSelf: "center", fontSize: 14 },
+											]}
+										>
+											{`Team\npage`}
+										</Text>
+									</TouchableOpacity>
+
+									<TouchableOpacity
+										style={[
+											styles.small_button,
+											{
+												borderWidth: 0.5,
+												height: 46,
+												borderRadius: 5,
+												width: 55,
+												margin: 3,
+												marginTop: 0,
+											},
+										]}
+									>
+										<Text
+											style={[
+												styles.button_text,
+												{ alignSelf: "center", fontSize: 14 },
+											]}
+										>
+											{`Leave\nteam`}
+										</Text>
+									</TouchableOpacity>
+								</View>
+							</View>
+
+							<View
+								style={[
+									styles.container,
+									{
+										flex: 1,
+										flexDirection: "row",
+										justifyContent: "space-evenly",
+									},
+								]}
+							></View>
+						</View>
+					);
+				})}
+				<TouchableOpacity
 					style={[
-						styles.container,
+						styles.small_button,
 						{
-							flex: 1,
-							flexDirection: "row",
-							justifyContent: "space-evenly",
+							borderWidth: 0.5,
+							height: 35,
+							borderRadius: 12,
+							width: 200,
 						},
 					]}
+					onPress={() => navigation.navigate("CreateTeam")}
 				>
-					<TouchableOpacity
-						style={[
-							styles.small_button,
-							{
-								borderWidth: 2,
-								height: 35,
-								borderRadius: 12,
-								width: 200,
-							},
-						]}
-						onPress={() => navigation.navigate("CreateTeam")}
+					<Text
+						style={[styles.button_text, { alignSelf: "center", fontSize: 18 }]}
 					>
-						<Text
-							style={[
-								styles.button_text,
-								{ alignSelf: "center", fontSize: 18 },
-							]}
-						>
-							Create a New Team
-						</Text>
-					</TouchableOpacity>
-				</View>
+						Create a New Team
+					</Text>
+				</TouchableOpacity>
 				<StatusBar />
 			</ScrollView>
 		</SafeAreaView>
